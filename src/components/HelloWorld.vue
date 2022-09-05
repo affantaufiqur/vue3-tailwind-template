0c331bcd659008b67d038b2993aad27788c02e9d
Affan <affantaufiqur@gmail.com>
2022-09-05 19:26:55 +0700

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="text-green-800 bg-green-100/[7] font-semibold">{{ msg }}</h1>
+  </div>
+</template>
